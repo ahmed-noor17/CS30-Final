89dd1e26007eeea1eac9e118cd45438e01da0df1
Aiden Dielschneider <aiden.dielschneider@rbe.sk.ca>
2024-12-16 11:38:52 -0600

    Added files for the attack and character classes

---
+++ attack.py
@@ -0,0 +1,4 @@
+class Attack:
+	def __init__(self, damage, use_text):
+		self.damage = damage
+		self.use_text = use_text
---
+++ character.py
@@ -0,0 +1,6 @@
+class Character:
+	def __init__(self, hp, atk, acc, moves):
+		self.hp = hp
+		self.atk = atk
+		self.acc = acc
+		self.moves = moves
