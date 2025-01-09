15c4a6c84632a5e6ff1e7fae308898f6349457b1
Aiden Dielschneider <aiden.dielschneider@gmail.com>
2025-01-08 18:33:31 -0600

    merged main.py and combat.py to fix an issue

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ import random
 import map as _map
 import inventory as _inventory
 import character as _character
-import combat as _combat
 import attack as _attack
 from tabulate import tabulate
 playing_game = True
@@ -23,8 +22,9 @@ game_title = '''
 |    -|   __|  |  |    -|   __|__   |__   |
 |__|__|_____|_____|__|__|_____|_____|_____|\n'''
 
+
 player = {
-	'character': _character.Character("john", 100, 5, 95, ['slash', 'fireball']),
+	'character': _character.Character("if you see this it is a bug!", 100, 5, 95, ['slash', 'fireball']),
 	'position': [1, 4, "house"],  # [x, y, map]
 	'inventory': _inventory.Inventory([None]),
     'enemies': {}
@@ -73,7 +73,9 @@ loaded_data = data_to_save.copy()
 
 current_save_file = None
 
-# Functions -------------------------------------------------------------------
+# Movement --------------------------------------------------------------------
+
+
 def up():
     print("You head north.")
     update_position("y", 1)
@@ -196,6 +198,9 @@ def current_room(y_offset=0, x_offset=0):
     return _map.game_map[player['position'][2]][int(player['position'][1]) + y_offset][int(player['position'][0]) + x_offset]  # map, y, x
 
 
+# Save/Load -------------------------------------------------------------------
+
+
 def save_data():
     ''' Writes data to save file.'''
     try:
@@ -215,11 +220,103 @@ def load_data():
                 if ", " in data[1]:
                     data[1] = data[1].split(", ")
                 loaded_data[data[0]] = data[1]
-        player['character'] = _character.Character(loaded_data['player_name'], loaded_data['player_max_hp'], loaded_data['player_atk'], loaded_data['player_acc'], loaded_data['player_moves'])
+        global player
+        player['character'] = _character.Character(loaded_data['player_name'], int(loaded_data['player_max_hp']), int(loaded_data['player_atk']), int(loaded_data['player_acc']), loaded_data['player_moves'])
         player['position'] = [int(loaded_data['player_x_position']), int(loaded_data['player_y_position']), loaded_data['player_map']]
-        player['inventory'] = _inventory.Inventory(loaded_data['player_inventory']),
+        player['inventory'] = _inventory.Inventory(loaded_data['player_inventory'])
     except FileNotFoundError:
         print("File does not exist.")
+
+
+# Combat ----------------------------------------------------------------------
+
+
+def enemy_turn():
+    for enemy in list(player['enemies'].keys()):
+        enemy_object = player['enemies'][enemy]
+        _print(f"\n{enemy_object.name.title()} took a turn!")
+        use_attack(attacks[enemy_object.moves[random.randint(0, len(enemy_object.moves) - 1)]], enemy_object, player['character'])
+        time.sleep(0.5)
+
+
+def combat(encounter_enemies):
+    for enemy in encounter_enemies:
+        enemy_object = _character.Character(enemies[enemy][0], enemies[enemy][1], enemies[enemy][2], enemies[enemy][3], enemies[enemy][4])
+        enemy_count = 1
+        while enemy_object.name in list(player['enemies'].keys()):
+            enemy_count += 1
+            if enemy_count > 2:  # This names the enemies if there are multiple of the same type
+                enemy_object.name = f"{enemy_object.name[:-2]} {str(enemy_count)}"
+            else:
+                enemy_object.name = f"{enemy_object.name} {str(enemy_count)}"
+        player['enemies'][enemy_object.name] = enemy_object
+        print(f"You encountered a {enemy.title()}!")
+
+    fighting = True
+    while fighting:
+        print(player['character'])
+        print(f"\nHP: {player['character'].hp}/{player['character'].max_hp}")
+        display_menu('combat_menu')
+        if len(list(player['enemies'].keys())) <= 0:
+            print("You won!")
+            fighting = False
+            break
+        enemy_turn()
+        if player['character'].hp <= 0:
+            print("You lost!")
+            fighting = False
+            #game_over()
+            break
+    player['enemies'].clear()
+
+
+def attack_menu():
+    print("Attacks:")
+    for attack in player['character'].moves:
+        print(f" - {attack.title()}")
+    while True:
+        use_move = input("Choose a move: ").lower()
+        if use_move in player['character'].moves and use_move in list(attacks.keys()):
+            while True:
+                #if len(list(player['enemies'].keys())) <= 1:
+                    #target = player['enemies'][0]
+                #else:
+                for target_enemy in list(player['enemies'].keys()):
+                    print(f" - {target_enemy.title()}")
+                target = input("Choose a target: ").lower()
+                if target in list(player['enemies'].keys()):
+                    use_attack(attacks[use_move], player['character'], player['enemies'][target])
+                    break
+            break
+
+
+def flee_battle():
+    print("You cannot flee yet!")
+    pass
+
+
+def use_item():
+    print("You cannot use items yet!")
+    pass
+
+
+def use_attack(attack, attacker, target):
+    attack_accuracy = attack.acc * attacker.acc/100
+    if random.randint(0, 100) <= attack_accuracy:
+        attack_damage = attack.damage * attacker.atk
+        target.hp -= attack_damage
+        _print(f"{attacker.name.title()} {attack.use_text.replace('{target}', target.name.title())}")
+        _print(f"Dealt {attack_damage} damage!")
+        _print(f"{target.name.title()} has {target.hp} health remaining!")
+        if target.hp <= 0:
+            print(f"Defeated {target.name.title()}!")
+            if target.name != player['character'].name:
+                player['enemies'].pop(target.name)
+    else:
+        print(f"{attacker.name.title()} missed! What an idiot!")
+
+
+# Base Functions --------------------------------------------------------------
 
 
 def setup():
@@ -321,7 +418,7 @@ def view_inventory():
 
 
 def fight_test():
-    _combat.combat(['orc'])
+    combat(['orc'])
 
 
 def _quit():
@@ -350,6 +447,7 @@ menu = {
     },
     "movement_menu": {"up": up, "down": down,
           "left": left, "right": right},
+    "combat_menu": {"attack": attack_menu, "item": use_item, "run": flee_battle}
 }
 
 
