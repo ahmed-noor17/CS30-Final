4522e01f3cb8b536cadfc7ef86a7960498bc6152
Ahmed Noor <ahmednoor011707@gmail.com>
2025-01-12 13:22:54 -0600

    Made descriptions for items and map

--- item_data.py
+++ item_data.py
@@ -4,59 +4,59 @@ item = {
     },
     "consumable": {
         "health potion": {
-            "description": "this is the item description",
+            "description": "A small vial of some green concoction. Gives moderate HP.",
             "cast": "heal",
             "value": 8},
         "scroll of healing": {
-            "description": "this is the item description",
+            "description": "Even just holding it gives a sense of relief. Gives major HP.",
             "cast": "super heal",
             "value": 20},
         "inferno scroll": {
-            "description": "this is the item description",
+            "description": "The paper is hot to the touch. Burns an enemy.",
             "cast": "incinerate",
             "value": 17},
         "cursed flame orb": {
-            "description": "this is the item description",
+            "description": "The cries of sacrificed souls burning to ash can be heard. Reduces an enemy to dust.",
             "cast": "cursed flame",
             "value": 58},
         "bottled lightning": {
-            "description": "this is the item description",
+            "description": "A bolt of lightning captured in a bottle. Strikes an enemy with a great lightning bolt.",
             "cast": "lightning bolt",
             "value": 25},
         "bottled thunder": {
-            "description": "this is the item description",
+            "description": "Dark clouds fill the interior of the bottle. Strikes all enemies with lightning.",
             "cast": "thunderstorm",
             "value": 20},
         "magic icicle": {
-            "description": "this is the item description",
+            "description": "Only freezes once it stabs a being. Freezes an enemy for one turn.",
             "cast": "freeze ray",
             "value": 22},
         "freeze orb": {
-            "description": "this is the item description",
+            "description": "A howling blizzard storm can be heard. Freezes an enemy for two turns.",
             "cast": "deep freeze",
-            "value": 58},
-        "frost scroll": {
-            "description": "this is the item description",
-            "cast": "frost blast",
             "value": 50},
+        "frost scroll": {
+            "description": "The paper is cold to the touch. Freezes all enemies for one turn.",
+            "cast": "frost blast",
+            "value": 60},
         "poison bomb": {
-            "description": "this is the item description",
+            "description": "A round bomb filled with the poisonous gas from the wastes. Poisons all enemies for two turns.",
             "cast": "poison cloud",
             "value": 20},
         "birthday bomb": {
-            "description": "Cheating is bad.",
+            "description": "Total annihilation.",
             "cast": "annihilation",
             "value": 1000000},
     },
     "equipment": {
         "iron sword": {
-            "description": "this is the item description",
+            "description": "The pride of an amateur smith.",
             "slot": "weapon",
             "value": 50}
     },
     "miscellaneous": {
         "silver ring": {
-            "description": "this is the item description",
+            "description": "A ring embued with magic. Boosts MP.",
             "value": 24}
     }
 }
