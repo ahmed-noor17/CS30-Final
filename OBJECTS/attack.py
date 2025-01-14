83754ad9baee1da8698014a8b2dc95219ece5340
Ahmed Noor <ahmednoor011707@gmail.com>
2025-01-14 11:50:33 -0600

    added more sounds

--- OBJECTS/attack.py
+++ OBJECTS/attack.py
@@ -1,8 +1,9 @@
 class Attack:
-	def __init__(self, damage, acc, use_text, target_type, debuff=None, debuff_stack_amount=0):
+	def __init__(self, damage, acc, use_text, target_type, debuff=None, debuff_stack_amount=0, sound='hit'):
 		self.damage = damage
 		self.acc = acc
 		self.use_text = use_text
 		self.target_type = target_type
 		self.debuff = debuff
 		self.debuff_stack_amount = debuff_stack_amount
+		self.sound = sound
