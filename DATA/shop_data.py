c001d475e8726e0f4079edc4a3ab9f1eae0eaaa5
Aiden Dielschneider <aiden.dielschneider@gmail.com>
2025-01-18 00:46:13 -0600

    shop_data

---
+++ DATA/shop_data.py
@@ -0,0 +1,59 @@
+shops = {
+    "dan's thingamabobs": {
+        "intro": 'An eccentric man stands behind the counter. He has a wooden sign wrapped around his neck with "DAN" scrawled on it.',
+        "wares":
+            ["health potion",
+            "magic icicle",
+            "inferno scroll",
+            "bottled lightning",
+            "birthday bomb"],
+        "dialogue":
+            ["Hey would you be willing to buy this here thingamajig? Actually, wait no, that's not for sale, nevermind...",
+            "Please buy my stuff. I was gravedigging all night yesterday! Hehehe, I jest. I found these in my cellar.",
+            "Y'know, if you're ever in a pinch in combat, these items are a real lifesaver, you know.",
+            "If you're ever in the area, would you mind checking out the sewer for me? I'll buy whatever you dredge up from there!",
+            "My cousin had a birthday party last week and I tried using the birthday bomb. It didn't go very well..."]
+    },
+    "blacksmith": {
+        "intro": 'A burly man welcomes you despite his attention clearly being on the lump of metal he is handling.',
+        "wares":
+            ['iron chestplate', 
+             'iron helmet', 
+             'iron sword', 
+             'iron mace', 
+             'iron buckler', 
+             'bow', 
+             'red ring'],
+        "dialogue":
+            ["Buy something. Or don't, I guess.",
+            "I used to be an adventurer like you, but then I took an arrow to the elbow.",
+            "Feel free to come back and thank me when my stuff ends up saving your skin."]
+    },
+    "black market": {
+        "intro": 'Men hiding behind leaves stare at you.',
+        "wares":
+            ["poison bomb",
+            "iron sword",
+            "iron mace",
+            "bottled lightning",
+            "cursed flame orb"],
+        "dialogue":
+            ["Are you looking to buy... or sell..?",
+            "No questions. Just business.",
+            "If you're not here for business, then scram!",
+            "Were you followed here?",
+            "Don't tell me you're one of the king's rats...",
+            "I'd hide here if you don't want to get caught.",
+            "...You don't have any coin, do you?"]
+    },
+    "tree's shop": {
+        "intro": 'You find it a bit odd buying things from a tree.',
+        "wares":
+            ["scroll of healing",
+            "frost scroll",
+            "inferno scroll",
+            "poison bomb"],
+        "dialogue":
+            ["...", "......", "....(You feel like the tree is judging you)"]
+    }
+}
